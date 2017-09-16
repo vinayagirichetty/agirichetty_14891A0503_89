755745d2da967607656fe8ce5ec0b7b833ada17d
vinayagirichetty <32007720+vinayagirichetty@users.noreply.github.com>
2017-09-16 12:51:39 +0530

    Add files via upload

--- src/StudentGroup.java
+++ src/StudentGroup.java
@@ -106,8 +106,11 @@ public class StudentGroup implements StudentArrayOperation {
 	@Override
 	public void removeFromIndex(int index) {
 		// Add your implementation here
+		if (student == null ) throw new IllegalArgumentException();
+		else {
 		ArrayList <Student>al = new ArrayList<Student>();
 		al.remove(index);
+		}
 	}
 
 	@Override
